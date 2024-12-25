aa90ad4daa08e123f73b69ec7ab0e9c0374bd65a
Chukwudera Amalu <chukwuderaamalu@gmail.com>
2024-12-25 22:24:13 +0100

    christmas sales

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+function ticketFee () {
+
+   
+
+    const input = document.querySelector(`.inputElement`)
+    let age = input.value
+       
+    if(age > 0 && age <= 12) {
+          ticketPrice = `Your ticket price is $${5}`;
+      
+    }
+    else if(age > 12 && age <= 18 ) {
+        ticketPrice = `Your ticket price is $${10}`;
+       
+    }
+    else if(age > 18 && age <= 60 ) {
+         ticketPrice = `Your ticket price is $${20}`;
+       
+    }
+    else if( age > 60 ) {
+       ticketPrice = `Your ticket price is $${15}`;
+       
+        
+    }
+    
+    input.value =``
+    document.querySelector(`.ticket`).innerHTML = ticketPrice
+    
+
+
+   
+}
+
+
+
+
+
+function clearButton (){
+    
+    
+    document.querySelector(`.ticket`).innerHTML = ``
+    
+}
